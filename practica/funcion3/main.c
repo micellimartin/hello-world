7ba6759f76614007bf9f7feed4d1ac89eb429da7
alumno <alumno>
2019-03-22 22:29:44 -0300

    commit

---
+++ practica/funcion3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumar3(); //no recibe nada, es lo mismo dejarlo vacio cuando recibo, pero si devuelve algo
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
